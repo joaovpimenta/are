4b391560da3ee266a0d544a442b4791500e6250e
João Victor <52414666+joaovpimenta@users.noreply.github.com>
2026-09-08 09:58:53 -0300

    feat(engine): export initial components

---
+++ engine/src/index.ts
@@ -0,0 +1,6 @@
+export { Keypad3D } from './components/keypad/Keypad3D';
+export { createKeypadMachine } from './components/keypad/keypadMachine';
+export { Dial3D } from './components/dial/Dial3D';
+export { useGameStore } from './store/gameStore';
+export { amberTheme, defaultTheme } from './theme';
+export type { AreTheme } from './theme';
